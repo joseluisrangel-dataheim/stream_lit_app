d465d182b96a25e541e5cb717cd82c869a871b0b
JLRangel <joseluis.rangel@dataheim.live>
2020-09-08 17:15:41 -0500

    Archivo base para la app

--- first_app.py
+++ first_app.py
@@ -2,3 +2,37 @@ import streamlit as st
 
 import numpy as np
 import pandas as pd
+
+
+st.title('My first app')
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+st.line_chart(chart_data)
+
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    st.line_chart(chart_data)
+
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
